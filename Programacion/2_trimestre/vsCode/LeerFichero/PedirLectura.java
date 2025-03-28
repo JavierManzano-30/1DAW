3e5521ac78ba71ce497fafb583783f0247d90415
JavierManzano-30 <javier.manzanooliveros@adaits.es>
2025-03-28 17:07:18 +0100

    Programacion-LeerFichero

--- Programacion/2_trimestre/vsCode/LeerFichero/PedirLectura.java
+++ Programacion/2_trimestre/vsCode/LeerFichero/PedirLectura.java
@@ -1,6 +1,24 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class PedirLectura {
     
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        String word = args[0];
+        int times = Integer.parseInt(args[1]);
+
+        try {
+            FileWriter escritor = new FileWriter("./repeat.txt");
+            for (int i = 0; i < times; i+=1) {
+                escritor.write(word + "\n");
+                //escritor.flush();
+                Thread.sleep(1000);
+            }
+            escritor.close();
+        } catch (IOException | InterruptedException | ArrayIndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Done");
     }
 }
---
+++ Programacion/2_trimestre/vsCode/LeerFichero/hastaPonerFin.java
@@ -0,0 +1,5 @@
+public class hastaPonerFin {
+    public static void main(String[] args) {
+        
+    }
+}
